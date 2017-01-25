ac9083774f615a7ac81eb306ba9023049eaa9fc9
Zach Munger <zmunger@Zacharys-MacBook-Pro.local>
2017-01-24 20:05:33 -0800

    Change kernel to BF

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,7 +26,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 features_train = features_train[:len(features_train)/100]
 labels_train = labels_train[:len(labels_train)/100]
-clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf")
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
