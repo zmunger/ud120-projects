e778fb00de9b6d25663880e8e60b0132b1794bf4
Zach Munger <zmunger@Zacharys-MacBook-Pro.local>
2017-01-24 19:59:19 -0800

    Add code to shrink training data set - decreases Acc.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 from sklearn.svm import SVC
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 clf = SVC(kernel="linear")
 clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
