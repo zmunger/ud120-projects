aa4965505c47d40678285e4a9666584a07b24f58
Zach Munger <zmunger@Zacharys-MacBook-Pro.local>
2017-01-25 15:05:28 -0800

    Get decision tree running with accuracy

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -23,9 +23,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
 #########################################################
 
 
